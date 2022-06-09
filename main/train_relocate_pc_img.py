b6cefb2d43e08d928660953d9cef5b6b5bf81389
yzqin <yzqin1996@gmail.com>
2022-06-09 02:28:32 -0700

    [update] update img main file

--- main/train_relocate_pc_img.py
+++ main/train_relocate_pc_img.py
@@ -15,12 +15,14 @@ if __name__ == '__main__':
     parser.add_argument('--n', type=int, default=100)
     parser.add_argument('--workers', type=int, default=10)
     parser.add_argument('--lr', type=float, default=3e-4)
-    parser.add_argument('--ep', type=int, default=5)
+    parser.add_argument('--ep', type=int, default=10)
     parser.add_argument('--bs', type=int, default=1000)
     parser.add_argument('--seed', type=int, default=100)
     parser.add_argument('--iter', type=int, default=1000)
+    parser.add_argument('--randomness', type=float, default=1.0)
     parser.add_argument('--exp', type=str)
     parser.add_argument('--object_name', type=str)
+    parser.add_argument('--object_cat', default="YCB", type=str)
     parser.add_argument('--use_bn', type=bool, default=True)
     parser.add_argument('--img_type',
                         default='all',
@@ -30,12 +32,25 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     object_name = args.object_name
-    exp_keywords = ["ppo_imagination", args.img_type, object_name, args.exp, str(args.seed)]
-    env_iter = args.iter * 500 * args.n
+    object_cat = args.object_cat
+    randomness = args.randomness
+    exp_keywords = ["ppo_img", args.img_type, object_name, args.exp, str(args.seed)]
+    horizon = 200
+    env_iter = args.iter * horizon * args.n
+
+    config = {
+        'n_env_horizon': args.n,
+        'object_name': object_name,
+        'object_category': object_cat,
+        'update_iteration': args.iter,
+        'total_step': env_iter,
+        'randomness': randomness,
+    }
 
     exp_name = "-".join(exp_keywords)
     result_path = Path("./results") / exp_name
     result_path.mkdir(exist_ok=True, parents=True)
+    wandb_run = setup_wandb(config, exp_name, tags=["imagination", "relocate", object_name])
 
     if args.img_type == "robot":
         img_config = task_setting.IMG_CONFIG["relocate_robot_only"]
@@ -51,13 +66,15 @@ if __name__ == '__main__':
 
 
     def create_env_fn():
-        environment = create_relocate_env(object_name, use_visual_obs=True)
+        environment = create_relocate_env(object_name, use_visual_obs=True, object_category=object_cat,
+                                          randomness_scale=randomness)
         environment.setup_imagination_config(img_config)
         return environment
 
 
     def create_eval_env_fn():
-        environment = create_relocate_env(object_name, use_visual_obs=True, is_eval=True)
+        environment = create_relocate_env(object_name, use_visual_obs=True, is_eval=True, object_category=object_cat,
+                                          randomness_scale=randomness)
         environment.setup_imagination_config(img_config)
         return environment
 
@@ -69,8 +86,8 @@ if __name__ == '__main__':
     feature_extractor_class = PointNetImaginationExtractor
     feature_extractor_kwargs = {
         "pc_key": "relocate-point_cloud",
-        "local_channels": (64, 128, 512),
-        "global_channels": (512, 256),
+        "local_channels": (64, 128, 256),
+        "global_channels": (256, ),
         "imagination_keys": imagination_keys,
         "use_bn": args.use_bn,
     }
@@ -83,17 +100,15 @@ if __name__ == '__main__':
 
     config = {'n_env_horizon': args.n, 'object_name': args.object_name, 'update_iteration': args.iter,
               'total_step': env_iter, "use_bn": args.use_bn, "policy_kwargs": policy_kwargs}
-    wandb_run = setup_wandb(config, exp_name, tags=["imagination", "relocate", object_name])
 
     model = PPO("PointCloudPolicy", env, verbose=1,
                 n_epochs=args.ep,
-                n_steps=(args.n // args.workers) * 500,
+                n_steps=(args.n // args.workers) * horizon,
                 learning_rate=args.lr,
                 batch_size=args.bs,
                 seed=args.seed,
                 policy_kwargs=policy_kwargs,
-                tensorboard_log=str(result_path / "log"),
-                min_lr=1e-4,
+                min_lr=args.lr,
                 max_lr=args.lr,
                 adaptive_kl=0.02,
                 target_kl=0.1,
