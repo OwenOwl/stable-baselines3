facb109c6ed2b6b195bfb851f013b8e1bab13319
OwenOwl <flx260085323@163.com>
2023-05-10 11:44:32 -0700

    move out reward coef

--- main/train_pure_imitate.py
+++ main/train_pure_imitate.py
@@ -1,23 +1,25 @@
 from pathlib import Path
 
 import torch.nn as nn
+import numpy as np
 
 from hand_env_utils.arg_utils import *
 from hand_env_utils.wandb_callback import WandbCallback, setup_wandb
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 from stable_baselines3.ppo import PPO
 
-def create_env(object_name, use_visual_obs, object_category="HOI4D", use_gui=False, is_eval=False,
-                           randomness_scale=1, pc_noise=True):
-    import os
+from datetime import datetime
 
+def create_env(use_visual_obs, use_gui=False, is_eval=False,
+               reward_args=np.zeros(5), randomness_scale=1, pc_noise=True):
+    import os
     from hand_teleop.env.rl_env.imitation_env import ImitationEnv
     from hand_teleop.real_world import task_setting
     from hand_teleop.env.sim_env.constructor import add_default_scene_light
     frame_skip = 1
-    env_params = dict(use_gui=use_gui, frame_skip=frame_skip, no_rgb=True)
+    env_params = dict(reward_args=reward_args, use_gui=use_gui, frame_skip=frame_skip, no_rgb=True)
     if is_eval:
-        env_params["no_rgb"] = False
+        env_params["no_rgb"] = False 
         env_params["need_offscreen_render"] = True
 
     # Specify rendering device if the computing device is given
@@ -46,21 +48,19 @@ if __name__ == '__main__':
     parser.add_argument('--iter', type=int, default=5000)
     parser.add_argument('--randomness', type=float, default=1.0)
     parser.add_argument('--exp', type=str)
-    parser.add_argument('--object_name', type=str, default="laptop")
-    parser.add_argument('--object_cat', default="HOI4D", type=str)
+    parser.add_argument('--reward', type=float, nargs="+", default=[10,0.5,0.1,10,0.5])
 
     args = parser.parse_args()
-    object_name = args.object_name
-    object_cat = args.object_cat
     randomness = args.randomness
-    exp_keywords = ["ppo", args.exp, str(args.seed)]
+    now = datetime.now()
+    exp_keywords = ["ppo", args.exp, ",".join(str(i) for i in args.reward)]
     horizon = 200
     env_iter = args.iter * horizon * args.n
+    reward_args = args.reward
+    assert(len(reward_args) >= 5)
 
     config = {
         'n_env_horizon': args.n,
-        'object_name': object_name,
-        'object_category': object_cat,
         'update_iteration': args.iter,
         'total_step': env_iter,
         'randomness': randomness,
@@ -69,16 +69,14 @@ if __name__ == '__main__':
     exp_name = "-".join(exp_keywords)
     result_path = Path("./results") / exp_name
     result_path.mkdir(exist_ok=True, parents=True)
-    wandb_run = setup_wandb(config, exp_name, tags=["state", "imitation", object_name])
+    wandb_run = setup_wandb(config, "-".join([exp_name, now.strftime("(%Y/%m/%d,%H:%M)")]), tags=["state", "imitation"])
 
     def create_env_fn():
-        environment = create_env(object_name, use_visual_obs=False, object_category=object_cat,
-                                             randomness_scale=randomness)
+        environment = create_env(use_visual_obs=False, reward_args=reward_args, randomness_scale=randomness)
         return environment
     
     def create_eval_env_fn():
-        environment = create_env(object_name, use_visual_obs=False, is_eval=True, object_category=object_cat,
-                                             randomness_scale=randomness)
+        environment = create_env(use_visual_obs=False, reward_args=reward_args, is_eval=True, randomness_scale=randomness)
         return environment
 
     env = SubprocVecEnv([create_env_fn] * args.workers, "spawn")
